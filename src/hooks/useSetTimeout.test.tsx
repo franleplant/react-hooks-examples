9aa1ef8b6676c91e2b0a44ec038d21b8e33b0bc0
Fran Guijarro <franleplant@gmail.com>
2020-09-29 23:22:46 -0300

    autoformat

--- src/hooks/useSetTimeout.test.tsx
+++ src/hooks/useSetTimeout.test.tsx
@@ -5,46 +5,50 @@ import { renderHook } from "@testing-library/react-hooks";
 import useSetTimeout from "./useSetTimeout";
 
 function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 describe.only("useSetTimeout", () => {
   it("works as setTimeout", async () => {
     const { result } = renderHook(() => useSetTimeout());
-    const customSetTimeout = result.current
+    const customSetTimeout = result.current;
 
-    const isCalled: Array<boolean> = []
-    const callback = (index: number) => () => {isCalled[index] = true}
-    const time = 100
+    const isCalled: Array<boolean> = [];
+    const callback = (index: number) => () => {
+      isCalled[index] = true;
+    };
+    const time = 100;
 
-    customSetTimeout(callback(0), time)
-    customSetTimeout(callback(1), time)
-    customSetTimeout(callback(2), time)
+    customSetTimeout(callback(0), time);
+    customSetTimeout(callback(1), time);
+    customSetTimeout(callback(2), time);
 
     await wait(() => {
-      expect(isCalled[0]).toBe(true)
-      expect(isCalled[1]).toBe(true)
-      expect(isCalled[2]).toBe(true)
-    })
+      expect(isCalled[0]).toBe(true);
+      expect(isCalled[1]).toBe(true);
+      expect(isCalled[2]).toBe(true);
+    });
   });
 
   it("doesn't run when the component is unmounted", async () => {
     const { result, unmount } = renderHook(() => useSetTimeout());
-    const customSetTimeout = result.current
+    const customSetTimeout = result.current;
 
-    const isCalled: Array<boolean> = [false, false, false]
-    const callback = (index: number) => () => { isCalled[index] = true }
-    const time = 100
+    const isCalled: Array<boolean> = [false, false, false];
+    const callback = (index: number) => () => {
+      isCalled[index] = true;
+    };
+    const time = 100;
 
-    customSetTimeout(callback(0), time)
-    customSetTimeout(callback(1), time)
-    customSetTimeout(callback(2), time)
+    customSetTimeout(callback(0), time);
+    customSetTimeout(callback(1), time);
+    customSetTimeout(callback(2), time);
 
-    unmount()
+    unmount();
     // wait for a decent amount of time
-    await sleep(time * 2)
-      expect(isCalled[0]).toBe(false)
-      expect(isCalled[1]).toBe(false)
-      expect(isCalled[2]).toBe(false)
+    await sleep(time * 2);
+    expect(isCalled[0]).toBe(false);
+    expect(isCalled[1]).toBe(false);
+    expect(isCalled[2]).toBe(false);
   });
 });
--- src/hooks/useTimeout.test.tsx
+++ src/hooks/useTimeout.test.tsx
@@ -5,70 +5,80 @@ import { renderHook } from "@testing-library/react-hooks";
 import useTimeout from "./useTimeout";
 
 function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 describe("useTimeout", () => {
   it("works as useTimeout", async () => {
-    let isCalled = false
-    const callback = () => {isCalled = true}
-    const time = 100
+    let isCalled = false;
+    const callback = () => {
+      isCalled = true;
+    };
+    const time = 100;
     const { result } = renderHook(() => useTimeout(callback, time));
 
-    await wait(() => expect(isCalled).toBe(true))
+    await wait(() => expect(isCalled).toBe(true));
   });
 
   it("doesn't run when the component is unmounted", async () => {
-    let isCalled = false
-    const callback = () => {isCalled = true}
-    const time = 100
+    let isCalled = false;
+    const callback = () => {
+      isCalled = true;
+    };
+    const time = 100;
     const { result, unmount } = renderHook(() => useTimeout(callback, time));
 
-    unmount()
+    unmount();
     // wait for a decent amount of time
-    await sleep(time * 2)
-    expect(isCalled).toBe(false)
+    await sleep(time * 2);
+    expect(isCalled).toBe(false);
   });
 
   it("doesn't reset timer when callback changes", async () => {
-    let isCalled = false
-    let callback = () => {}
-    const time = 100
+    let isCalled = false;
+    let callback = () => {};
+    const time = 100;
     const { result, rerender } = renderHook(() => useTimeout(callback, time));
-    callback = () => {isCalled = true}
-    rerender()
+    callback = () => {
+      isCalled = true;
+    };
+    rerender();
     // wait for the timer plus some minimal offset
-    await sleep(time  + 10)
+    await sleep(time + 10);
 
-    expect(isCalled).toBe(true)
+    expect(isCalled).toBe(true);
   });
 
   it("resets timer when the time changes", async () => {
-    let isCalled = false
-    const callback = () => {isCalled = true}
-    let time = 100
+    let isCalled = false;
+    const callback = () => {
+      isCalled = true;
+    };
+    let time = 100;
     const { result, rerender } = renderHook(() => useTimeout(callback, time));
     // set the time so that the previous timer is cleared
     // and the new one is called faster
-    time = 1
-    rerender()
+    time = 1;
+    rerender();
 
     // we wait for the new time plus some minimal offset
-    await sleep(time  + 10)
-    expect(isCalled).toBe(true)
+    await sleep(time + 10);
+    expect(isCalled).toBe(true);
   });
 
   it("clears the timer and doesn't set a new one when the time is undefined", async () => {
-    let isCalled = false
-    const callback = () => {isCalled = true}
-    const originalTime = 100
-    let time = originalTime
+    let isCalled = false;
+    const callback = () => {
+      isCalled = true;
+    };
+    const originalTime = 100;
+    let time = originalTime;
     const { result, rerender } = renderHook(() => useTimeout(callback, time));
-    time = undefined
-    rerender()
+    time = undefined;
+    rerender();
 
     // we wait for the new time plus some minimal offset
-    await sleep(originalTime + 10)
-    expect(isCalled).toBe(false)
+    await sleep(originalTime + 10);
+    expect(isCalled).toBe(false);
   });
 });
